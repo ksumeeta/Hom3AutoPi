80b6dc9b8c18e412020adb09c96f31f77d8f8035
root <ksumeeta@gmail.com>
2016-10-06 11:10:53 +0300

    Mesh Server Pi Cleaned

--- MeshServer/Pi/RF24MeshServer.cpp
+++ MeshServer/Pi/RF24MeshServer.cpp
@@ -1,6 +1,4 @@
- 
- 
- /** RF24Mesh_Example_Master.ino by TMRh20
+/** RF24Mesh_Example_Master.ino by TMRh20
   * 
   * Note: This sketch only functions on -Arduino Due-
   *
@@ -24,6 +22,7 @@ struct DeviceT{
 	uint8_t Type;
 	uint8_t Ver;
 	uint8_t EncrKey[9];
+	uint8_t RandomID;
 };
 
 DeviceT Device;
@@ -36,70 +35,73 @@ RF24Mesh mesh(radio,network);
 
 uint32_t displayTimer=0;
 
+bool FindFreeNodeID(int k){
+	for(int i=0; i<mesh.addrListTop; i++){
+		if(mesh.addrList[i].nodeID!=i+1){
+			printf("%u\n",i+1);
+			break;
+		}else if(mesh.addrListTop==i+1){
+			printf("%u\n",i+1);
+			break;
+		}
+	}
+}
+
+
+
 int main(int argc, char** argv) {
-  
-  
-  
-  
-  
-  // Set the nodeID to 0 for the master node
-  mesh.setNodeID(0);
-  // Connect to the mesh
-  printf("starting...\n");
-  mesh.begin();
-  //radio.setPALevel(RF24_PA_MAX);  
-  radio.printDetails();
+	mesh.setNodeID(0);
+	printf("starting...\n");
+	mesh.begin();
+	radio.setPALevel(RF24_PA_MAX);  
+	radio.printDetails();
 
-
-while(1)
-{
-  
-  // Call network.update as usual to keep the network updated
-  mesh.update();
-
-  // In addition, keep the 'DHCP service' running on the master node so addresses will
-  // be assigned to the sensor nodes
-  mesh.DHCP();
-  
-  
-  // Check for incoming data from the sensors
-  while(network.available()){
-//    printf("rcv\n");
-    RF24NetworkHeader header;
-    network.peek(header);
-    
-    uint32_t dat=0;
-    switch(header.type){
-      // Display the incoming millis() values from the sensor nodes
-      case 'M': network.read(header,&dat,sizeof(dat)); 
-                printf("Received From-->%u Value-->%u\n",mesh.getNodeID(header.from_node),dat);
-                 break;
-      case 0x7D:
-   		network.read(header,&Device,sizeof(Device)); 
-                printf("Join Request From-->%u DeviceID-->%u DeviceType-->%u DeviceVersion-->%u\n",mesh.getNodeID(header.from_node),Device.NodeID,Device.Type,Device.Ver);
-                 break;
-      
-      default:  network.read(header,0,0); 
-                printf("Rcv bad Data -->%u Type-->%d\n",mesh.getNodeID(header.from_node),header.type); 
-                break;
-    }
-  }
-  if(millis() - displayTimer > 5000){
-    displayTimer = millis();
-    printf("\n");
-    printf("********Assigned Addresses********\n");
-    for(int i=0; i<mesh.addrListTop; i++){
-       printf("NodeID: ");
-       printf("%u",mesh.addrList[i].nodeID);
-       printf(" RF24Network Address: ");
-       printf("%o",mesh.addrList[i].address);
-       printf("\n");
-     }
-    printf("********Assigned Addresses********\n\n");
-  }
-  
-  }
-return 0;
+	while(1){
+		mesh.update();
+		mesh.DHCP();
+		while(network.available()){
+			RF24NetworkHeader header;
+			network.peek(header);
+			
+			uint32_t dat=0;
+			switch(header.type){
+				case 'M': network.read(header,&dat,sizeof(dat)); 
+					printf("Received From-->%u Value-->%u\n",mesh.getNodeID(header.from_node),dat);
+					break;
+				case 0x7D:
+					network.read(header,&Device,sizeof(Device)); 
+					printf("Join Request From-->%u DeviceID-->%u DeviceType-->%u DeviceVersion-->%u RandomID-->%u\n",mesh.getNodeID(header.from_node),Device.NodeID,Device.Type,Device.Ver,Device.RandomID);
+					for(int i=0; i<mesh.addrListTop; i++){
+						if(mesh.addrList[i].nodeID!=i+1){
+							printf("%u\n",i+1);
+							break;
+						}else if(mesh.addrListTop==i+1){
+							printf("%u\n",i+1);
+							break;
+						}
+					}
+					break;
+				default:  network.read(header,0,0); 
+					printf("Rcv bad Data -->%u Type-->%d\n",mesh.getNodeID(header.from_node),header.type); 
+					break;
+			}
+		}
+		if(millis() - displayTimer > 5000){
+			displayTimer = millis();
+			printf("\n");
+			printf("********Assigned Addresses********\n");
+			for(int i=0; i<mesh.addrListTop; i++){
+				printf("NodeID: ");
+				printf("%u",mesh.addrList[i].nodeID);
+				printf(" RF24Network Address: ");
+				printf("%o",mesh.addrList[i].address);
+				printf("\n");
+			}
+			printf("********Assigned Addresses********\n\n");
+		}
+	
+	}
+	return 0;
 }
 
       
