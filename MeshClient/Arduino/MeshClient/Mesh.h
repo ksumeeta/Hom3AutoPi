72a9129eaaa096cf1fd2e4e2a178db3d4836e069
Sumeet Kumar <ksumeeta@gmail.com>
2016-10-06 15:22:08 +0530

    cleaning

--- MeshClient/Arduino/MeshClient/Mesh.h
+++
@@ -1,9 +0,0 @@
-struct DeviceT1 {
-	uint8_t NodeID;
-	uint8_t Type;
-	uint8_t Ver;
-	uint8_t Channel;
-	uint8_t PaPower;
-	uint8_t EncrKey[9];
-	uint8_t RandomID;
-};
--- MeshClient/Arduino/MeshClient/MeshClient.ino
+++ MeshClient/Arduino/MeshClient/MeshClient.ino
@@ -10,8 +10,7 @@
 #include "RF24.h"
 #include "RF24Network.h"
 #include "RF24Mesh.h"
-#include "..\Mesh.h"
-#include "AES.h"
+#include "Mesh.h"
 #include <SPI.h>
 #include <EEPROM.h>
 
